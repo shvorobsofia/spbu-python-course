1b47977c4838f62b4fb587a3c020ba7ff9ed2d67
София Швороб <sofiasvorob@MacBook-Air-Sofia.local>
2025-02-03 14:45:10 +0300

    feat(generators): add RGBA generator and prime number sequence with decorator

---
+++ project/generators.py
@@ -0,0 +1,48 @@
+from itertools import product
+from typing import Generator, Callable
+
+
+def get_rgba_element(index: int):
+    """Генераторное выражение для четырехмерного набора векторов RGBA, где A - четное."""
+    rgba_space = ((r, g, b, a) for r, g, b, a in product(range(256), range(256), range(256), range(0, 101, 2)))
+
+    for i, value in enumerate(rgba_space):
+        if i == index:
+            return value
+    raise IndexError("Index out of range")
+
+
+def prime_generator() -> Generator[int, None, None]:
+    """Генератор простых чисел."""
+    primes = []
+    num = 2
+    while True:
+        if all(num % p != 0 for p in primes):
+            primes.append(num)
+            yield num
+        num += 1
+
+
+def prime_decorator(gen: Callable[[], Generator[int, None, None]]) -> Callable[[int], int]:
+    """Декоратор, превращающий генератор в функцию, возвращающую k-е простое число."""
+    cache = []
+    generator = gen()
+
+    def wrapper(k: int) -> int:
+        if k < 1:
+            raise ValueError("Index must be greater than or equal to 1")
+        while len(cache) < k:
+            cache.append(next(generator))
+        return cache[k - 1]
+
+    return wrapper
+
+
+@get_rgba_element
+def get_rgba(index: int):
+    return get_rgba_element(index)
+
+
+@prime_decorator
+def get_prime():
+    return prime_generator()
