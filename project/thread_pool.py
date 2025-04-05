82d4d3d240ef20ff52e404dda53300ae64e01530
Sem4kok <semkremnev@team.amocrm.com>
2025-04-05 17:49:47 +0300

    feat(ci): annotation added for mypy

--- project/thread_pool.py
+++ project/thread_pool.py
@@ -1,12 +1,14 @@
 import threading
 import queue
+from typing import Callable, Optional, Any, List
+
 
 class ThreadPool:
     def __init__(self, num_threads: int):
-        self.num_threads = num_threads
-        self.tasks = queue.Queue()
-        self.threads = []
-        self.shutdown_flag = threading.Event()
+        self.num_threads: int = num_threads
+        self.tasks: queue.Queue[tuple[Callable[[], Any], Optional[Callable[[Any], None]]]] = queue.Queue()
+        self.threads: List[threading.Thread] = []
+        self.shutdown_flag: threading.Event = threading.Event()
 
         for _ in range(num_threads):
             thread = threading.Thread(target=self._worker)
@@ -14,7 +16,7 @@ class ThreadPool:
             thread.start()
             self.threads.append(thread)
 
-    def _worker(self):
+    def _worker(self) -> None:
         while not self.shutdown_flag.is_set():
             try:
                 task, callback = self.tasks.get(timeout=1)  # Таймаут для проверки shutdown_flag
@@ -25,11 +27,11 @@ class ThreadPool:
             except queue.Empty:
                 continue
 
-    def enqueue(self, task, callback=None):
+    def enqueue(self, task: Callable[[], Any], callback: Optional[Callable[[Any], None]] = None) -> None:
         if not self.shutdown_flag.is_set():
             self.tasks.put((task, callback))
 
-    def dispose(self):
+    def dispose(self) -> None:
         self.shutdown_flag.set()
         for thread in self.threads:
             thread.join()
